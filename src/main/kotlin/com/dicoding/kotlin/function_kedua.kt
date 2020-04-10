999eb5e19e5e0015bfd34c117b9ace3466c705de
Riko Wijayanto <mr.rikowijayanto@gmail.com>
2020-04-10 17:37:04 +0700

    Revert "Adding new param into fold loop"
    This reverts commit 56bd05602390235c211625c2c016cbcff8026c50.

--- src/main/kotlin/com/dicoding/kotlin/function_kedua.kt
+++ src/main/kotlin/com/dicoding/kotlin/function_kedua.kt
@@ -1,32 +1,5 @@
 package com.dicoding.kotlin
 
-import java.lang.IllegalArgumentException
-
-//function dalam function
 fun main (){
-    println("Ini adalah function dalam function")
-    println("\n")
-    println("Percobaan dipanggil pertama")
-    val sumnilai = sum(3,6,3)
-    println("Nilainya adalah $sumnilai")
-
+    println("Ini adalah function yang kedua")
 }
-
-
-//function inside the function
-fun sum(value_a : Int, value_b : Int, value_c : Int) : Int {
-    fun Int.convert() {
-        if (this == 0) {
-            throw IllegalArgumentException("Value must bigger than 0")
-        }
-    }
-
-    value_a.convert()
-    value_b.convert()
-    value_c.convert()
-
-    return value_a+value_c
-}
-
-
-//fold function
