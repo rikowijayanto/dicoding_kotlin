9d4c0e81985d36636dff38d642b6676b9cead4a6
Riko Wijayanto <mr.rikowijayanto@gmail.com>
2020-04-10 16:59:18 +0700

    Add sum function

--- src/main/kotlin/com/dicoding/kotlin/function.kt
+++ src/main/kotlin/com/dicoding/kotlin/function.kt
@@ -1,5 +1,12 @@
 package com.dicoding.kotlin
 
 fun main () {
-    println("halo")
+    println("halo ini adalah program awal saya membuat fungsi")
+    val nilai_total = sum(3,4)
+    println("maka nilai totalnya adalah $nilai_total")
+}
+
+
+fun sum(value_a : Int, value_b :Int) : Int {
+    return value_a + value_b
 }
